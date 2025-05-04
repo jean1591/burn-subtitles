8dae9f51971bcfdca290b46b8cf475c5759c51ad
Jean Robertou <j-rob@live.fr>
2025-05-04 17:11:22 +0200

    feat(backend): add serve static module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,19 @@
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { QueueModule } from './queue/queue.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { StatusGateway } from './gateway/status.gateway';
 import { UploadModule } from './upload/upload.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
     }),
     UploadModule,
     QueueModule,
